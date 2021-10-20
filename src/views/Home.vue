20a044a09f7b671a56e328ab72b18929c7f22088
Meredith Matthews <meredith.lynn.matthews@gmail.com>
2021-10-19 22:01:02 -0400

    adding all the things

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Welcome!</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import Welcome from '../components/Welcome.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    //Welcome
+  }
+}
+</script>
---
+++ src/views/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="listing">
+    <div>This is the List page</div>
+    <input type='text'>
+  </div>
+</template>
+
+<script>
+
+import {getData} from '../services/getData';
+
+const data = getData();
+
+
+export default {
+  name: 'List',
+  components: {
+    //Welcome
+  }
+}
+
+</script>
