e873ae47c498f95589dafd1a815481aec7fd9a09
Meredith Matthews <meredith.lynn.matthews@gmail.com>
2021-10-22 15:58:32 -0400

    limit output to 10

--- src/views/List.vue
+++ src/views/List.vue
@@ -7,20 +7,20 @@
       <label class="form-label" for="search">
         Search
       </label>
-      <input id="search" class="form-control" type='text'>
+      <input id="search" class="form-control" type='text' v-model="searchstring">
     </div>
   </div>
   <div class="row">
     <h2>Total: {{ totalLength }} </h2>
-    <div class="col-4" v-for="(item, index) in data" :key="index">
+    <div class="col-4" v-for="(item, index) in data.slice(0,10)" :key="index">
       <div class="card">
         <div class="card-body">
           <div class="circle bg-primary">
-            <span class="initials text-white">{{ initials(item.name) }}</span>
+            <span class="initials text-white">{{ initials(item[1].name) }}</span>
           </div>
           <div class="d-inline-block">
-            <div class="card-title">{{ item.name }} ({{ index }})</div>
-            <p class="card-text">{{ item.subtitle }}</p>
+            <div class="card-title">{{ item[1].name }} ({{ item[0] }})</div>
+            <p class="card-text">{{ item[1].subtitle }}</p>
           </div>
         </div>
       </div>
@@ -65,19 +65,22 @@
 </style>
 
 <script>
-
 import {getData} from '../services/getData';
 
 export default {
   name: "List",
   data: function() {
     return {
-      data: getData(),
+      searchstring: ''
     };
   },
   computed: {
     totalLength() {
       return Object.keys(this.data).length;
+    },
+    data() {
+      var asArray = Object.entries(getData());
+      return asArray;
     },
   },
   methods: {
